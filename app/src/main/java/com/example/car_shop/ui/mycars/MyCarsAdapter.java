5a0b7a7af96ea82196769ddd8ef47023f7bb2ef4
Кубанычбек Куштарбеков <kushtarbekovkuba@gmail.com>
2022-05-25 23:15:17 +0600

    menu for seller, menu for client

--- app/src/main/java/com/example/car_shop/ui/mycars/MyCarsAdapter.java
+++ app/src/main/java/com/example/car_shop/ui/mycars/MyCarsAdapter.java
@@ -3,18 +3,23 @@ package com.example.car_shop.ui.mycars;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.car_shop.R;
 import com.example.car_shop.data.enums.Status;
+import com.example.car_shop.data.enums.UserRoles;
 import com.example.car_shop.data.models.Car;
 import com.example.car_shop.databinding.CarItemLayoutBinding;
 import com.example.car_shop.databinding.MyCarItemLayoutBinding;
 import com.example.car_shop.ui.cars.CarPageFragment;
 import com.example.car_shop.ui.cars.CarsFragment;
+import com.example.car_shop.userService.UserSingl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +51,45 @@ public class MyCarsAdapter extends RecyclerView.Adapter<MyCarsAdapter.CarHolder>
         holder.binding.status.setText("Статус: " + ((car.getStatus() == Status.SALE)?"В продаже":"Продано"));
 
         holder.binding.carImg.setImageBitmap(BitmapFactory.decodeByteArray(car.getPhoto(), 0, car.getPhoto().length));
+        holder.binding.dropdownMenu.setOnClickListener(v -> {
+            if (UserSingl.getUserSingln().getUserRole() == UserRoles.SELLER){
+                PopupMenu popupMenu = new PopupMenu(holder.binding.getRoot().getContext(), holder.binding.dropdownMenu);
+                popupMenu.getMenuInflater().inflate(R.menu.seller_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getTitle().toString()){
+                            case "Редактировать":
+                                return true;
+                            case "Продано":
+                                return true;
+                            case "На продажу":
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+            else if (UserSingl.getUserSingln().getUserRole() == UserRoles.CLIENT){
+                PopupMenu popupMenu = new PopupMenu(holder.binding.getRoot().getContext(), holder.binding.dropdownMenu);
+                popupMenu.getMenuInflater().inflate(R.menu.client_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getTitle().toString()){
+                            case "озвонить владельцу":
+                                return true;
+                            case "Удалить понравившееся":
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+
+        });
         holder.itemView.setOnClickListener(v -> {
             CarPageFragment carPageFragment = new CarPageFragment();
             Bundle bundle = new Bundle();
